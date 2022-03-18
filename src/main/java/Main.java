34630380faa8f3c548d191b4d8b0e127e54eb2ae
Anas-Noor <anasnoor243@gmail.com>
2022-03-18 18:44:54 +0500

    3rd Commit

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,7 +2,7 @@ import Service.ProductService;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Printing the Products");
+        System.out.println("Printing the Productss");
         ProductService pc = new ProductService();
     pc.getAllProducts().forEach(System.out::println);
     }
