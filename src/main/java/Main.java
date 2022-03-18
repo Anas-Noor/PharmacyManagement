f7e003a0556ac15444c7a602fd01cec898c7fe99
Anas-Noor <85308557+Anas-Noor@users.noreply.github.com>
2022-03-18 18:43:25 +0500

    Update Main.java

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,7 +2,7 @@ import Service.ProductService;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Printing All the Products");
+        System.out.println("Printing the Products");
         ProductService pc = new ProductService();
     pc.getAllProducts().forEach(System.out::println);
     }
