ada8d9f462a3a6468b1cfe614e6f09ff85e7b5e8
fahadkhan2001 <fk1271543@gmail.com>
2022-03-24 20:10:31 +0500

    new files

--- src/main/java/Service/FindMedicine.java
+++ src/main/java/Service/FindMedicine.java
@@ -1,25 +1,5 @@
 package Service;
 
 public class FindMedicine {
-    private String medicine_Name;
 
-    public FindMedicine(String medicine_Name) {
-        this.medicine_Name = medicine_Name;
-    }
-
-
-    @Override
-    public String toString() {
-        return "FindMedicine{" +
-                "medicine_Name='" + medicine_Name + '\'' +
-                '}';
-    }
-
-    public String getMedicine_Name() {
-        return medicine_Name;
-    }
-
-    public void setMedicine_Name(String medicine_Name) {
-        this.medicine_Name = medicine_Name;
-    }
 }
